ff0759e0ab1368ab8dce2201b3124b19428514a9
HarpreetSinghNagi <harpreetnagi456@gmail.com>
2023-04-12 21:37:28 +0530

    seprate setting model

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import "./Analytics.css"
+import './Analytics.css';
 import ReportTable from './ReportTable';
 import { fetchReportData, ReportData } from './api';
+import SettingsModal from './SettingModel';
 
 interface AnalyticsProps {
   apiUrl: string;
 }
 
-interface SettingsModalProps {
-  selectedColumns: string[];
-  setSelectedColumns: React.Dispatch<React.SetStateAction<string[]>>;
-  reportData: ReportData[];
- 
-}
-
-const Analytics: React.FC<AnalyticsProps> = ({ apiUrl }) => {
+export const Analytics: React.FC<AnalyticsProps> = ({ apiUrl }) => {
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
   const [reportData, setReportData] = useState<ReportData[]>([]);
@@ -47,61 +41,18 @@ const Analytics: React.FC<AnalyticsProps> = ({ apiUrl }) => {
 
   const toggleSettingsModal = () => {
     setShowSettingsModal(!showSettingsModal);
-  }
-
-  const SettingsModal: React.FC<SettingsModalProps> = ({ selectedColumns, setSelectedColumns, reportData }) => {
-    const [updatedSelectedColumns, setUpdatedSelectedColumns] = useState(selectedColumns);
-  
-    const handleColumnToggle = (column: string) => {
-      setUpdatedSelectedColumns(prevSelectedColumns => {
-        if (prevSelectedColumns.includes(column)) {
-          return prevSelectedColumns.filter(col => col !== column);
-        } else {
-          return [...prevSelectedColumns, column];
-        }
-      });
-    };
-  
-    
-  
-    const handleApplyChanges = () => {
-      setSelectedColumns(updatedSelectedColumns);
-      toggleSettingsModal();
-    };
-  
-    return (
-      <div className='analytics-settings-modal'>
-        <div className='analytics-settings-modal-header'>
-          <span>Column Settings</span>
-          <button className='analytics-settings-modal-close' onClick={toggleSettingsModal}>X</button>
-        </div>
-        <div className='analytics-settings-modal-content'>
-          {reportData.length > 0 && Object.keys(reportData[0]).map((key) => (
-            <div key={key} className='analytics-settings-modal-item'>
-              <input type='checkbox' id={key} checked={updatedSelectedColumns.includes(key)} onChange={() => handleColumnToggle(key)} />
-              <label htmlFor={key}>{key}</label>
-            </div>
-          ))}
-          <div className='analytics-settings-modal-footer'>
-          <button className='analytics-button analytics-button-apply' onClick={handleApplyChanges}>Apply Changes</button>
-        </div>
-        </div>
-        
- </div>
-    );
   };
-  
-
 
   return (
     <div className='analytics-container'>
       <h1 className='analytics-title'>Analytics</h1>
       <div className='analytics-date-range-picker-container'>
-
         <div className='analytics-date-range-picker'>
-          <label className='analytics-date-range-picker-label' htmlFor="start-date-picker">Start Date</label>
-          <DatePicker 
-            id="start-date-picker"
+          <label className='analytics-date-range-picker-label' htmlFor='start-date-picker'>
+            Start Date
+          </label>
+          <DatePicker
+            id='start-date-picker'
             selected={startDate}
             onChange={(date: Date) => setStartDate(date)}
             selectsStart
@@ -111,9 +62,11 @@ const Analytics: React.FC<AnalyticsProps> = ({ apiUrl }) => {
         </div>
 
         <div className='analytics-date-range-picker'>
-          <label className='analytics-date-range-picker-label' htmlFor="end-date-picker">End Date</label>
-          <DatePicker  className='analytics-date-range-picker'
-            id="end-date-picker"
+          <label className='analytics-date-range-picker-label' htmlFor='end-date-picker'>
+            End Date
+          </label>
+          <DatePicker
+            id='end-date-picker'
             selected={endDate}
             onChange={(date: Date) => setEndDate(date)}
             selectsEnd
@@ -122,30 +75,37 @@ const Analytics: React.FC<AnalyticsProps> = ({ apiUrl }) => {
             minDate={startDate}
           />
         </div>
-
       </div>
 
       <div className='analytics-buttons-container'>
-        <button className='analytics-button analytics-button-fetch-data' onClick={fetchData}>Fetch Data</button>
-        {reportData.length > 0 ? ( <button className='analytics-button analytics-button-settings' onClick={toggleSettingsModal} disabled={isLoading || reportData.length === 0}>
-   Settings
-  </button>) : null}
-        
+        <button className='analytics-button analytics-button-fetch-data' onClick={fetchData}>
+          Fetch Data
+        </button>
+        {reportData.length > 0 ? (
+          <button
+            className='analytics-button analytics-button-settings'
+            onClick={toggleSettingsModal}
+            disabled={isLoading || reportData.length === 0}
+          >
+            Settings
+          </button>
+        ) : null}
       </div>
 
       {isLoading && <div className='analytics-loading'>Loading...</div>}
-      {showSettingsModal &&  <SettingsModal 
+      {showSettingsModal && (
+        <SettingsModal
           selectedColumns={selectedColumns}
           setSelectedColumns={setSelectedColumns}
-          reportData={reportData}   />}
-      {!isLoading&& reportData.length > 0 ? (
-<ReportTable reportData={reportData} selectedColumns={selectedColumns} />
-) : (
-<div className='analytics-placeholder'>No data to display</div>
-)}
-  
-</div>
-);
-};
-
-export default Analytics
+          reportData={reportData}
+          toggleSettingsModal={toggleSettingsModal}
+        />
+      )}
+      {!isLoading && reportData.length > 0 ? (
+        <ReportTable reportData={reportData} selectedColumns={selectedColumns} />
+      ) : (
+        <div className='analytics-placeholder'>No data to display</div>
+      )}
+    </div>
+  )
+      };
