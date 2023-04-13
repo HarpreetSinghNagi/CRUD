a5d161617a961019bfbe37959379cd7c3acde1d4
HarpreetSinghNagi <harpreetnagi456@gmail.com>
2023-04-13 15:33:47 +0530

    format

--- src/ReportTable.tsx
+++ src/ReportTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReportData } from './api';
+import { ReportData, formatDate, formatNumber, formatPercentage} from './api';
 
 interface ReportTableProps {
     reportData: ReportData[];
@@ -20,7 +20,21 @@ interface ReportTableProps {
           {reportData.map((row, index) => (
             <tr key={index}>
               {selectedColumns.map((column) => (
-                <td key={column}>{row[column]}</td>
+                  <td key={column}>
+                  {column === 'date'
+                    ? formatDate(row[column])
+                    : column === 'request' || column === 'clicks'
+                    ? formatNumber(row[column])
+                    : column === 'response' || column === 'impression'
+                    ? formatNumber(row[column])
+                    : column === 'revenue'
+                    ? `₹${row[column].toFixed(2)}`
+                    : column === 'Fill Rate'
+                    ? formatPercentage((row['AD Response'] / row['AD Request']) * 100)
+                    : column === 'ctr'
+                    ? formatPercentage((row['Clicks'] / row['Impression']) * 100)
+                    : row[column]}
+                </td>
               ))}
             </tr>
           ))}
